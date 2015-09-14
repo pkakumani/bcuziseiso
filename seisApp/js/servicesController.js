355d014cf54a48b5fdca14c8a86eaa06b2290937
unknown <PKakumani@SEA424F0T6.DirectApps.int>
2015-09-14 11:07:17 -0700

    updates

--- seisApp/js/servicesController.js
+++
@@ -1,7 +0,0 @@
-sesioApp.controller('servicesController', ["$scope", function ($scope) {
-	/*services tabs default set to "services"*/
-	$scope.servicesModel = "services";
-
-	$scope.formFilds = {searchName:"Text Entry", email1:"",email2:"me@evagiselle.co",email3:"me@evagiselle.com" ,platform:""}
-
-}]);
